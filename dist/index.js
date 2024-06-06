ccdc81c4dccd26cdb6dc21cc79c784e971d2124e
Shane <6071159+smashedr@users.noreply.github.com>
2024-06-06 11:55:12 -0700

    Test

--- dist/index.js
+++ dist/index.js
@@ -33619,29 +33619,37 @@ const semver = __nccwpck_require__(1383)
 
 ;(async () => {
     try {
-        // console.log('-'.repeat(40))
-        // console.log('process.env:', process.env)
-        // console.log('-'.repeat(40))
-        // console.log('github.context', github.context)
-        // console.log('-'.repeat(40))
-        // console.log('release', github.context.payload.release)
+        console.log('-'.repeat(40))
+        console.log('process.env:', process.env)
+        console.log('-'.repeat(40))
+        console.log('-'.repeat(40))
+        console.log('github.context', github.context)
+        console.log('-'.repeat(40))
         console.log('-'.repeat(40))
 
         // Check Release
         if (!github.context.payload.release) {
             core.info(`Skipping non-release: ${github.context.eventName}`)
-            return
+            // return
         }
 
         // Process Inputs
         const githubToken = core.getInput('token')
-        console.log('token:', githubToken)
+        // console.log('token:', githubToken)
         const tagPrefix = core.getInput('prefix')
         console.log('prefix:', tagPrefix)
         const updateMajor = core.getInput('major')
         console.log('major:', updateMajor)
         const updateMinor = core.getInput('minor')
         console.log('minor:', updateMinor)
+
+        const inputs = await github.context.getInputs()
+        console.log('inputs:', inputs)
+        // const parsedTags = parse(tagsInput, {
+        //     delimiter: ",",
+        //     trim: true,
+        //     relax_column_count: true,
+        // }).flat();
 
         // Set Variables
         const { owner, repo } = github.context.repo
@@ -33656,42 +33664,42 @@ const semver = __nccwpck_require__(1383)
         const minor = semver.minor(tag_name)
         console.log('minor', minor)
 
-        // Collect Tags
-        const tags = []
-        if (updateMajor !== 'false') {
-            tags.push(`${tagPrefix}${major}`)
-        }
-        if (updateMinor !== 'false') {
-            tags.push(`${tagPrefix}${major}.${minor}`)
-        }
-        console.log('tags', tags)
-        if (!tags.length) {
-            core.notice('Major and Minor false, nothing to do!')
-            return
-        }
+        // // Collect Tags
+        // const tags = []
+        // if (updateMajor !== 'false') {
+        //     tags.push(`${tagPrefix}${major}`)
+        // }
+        // if (updateMinor !== 'false') {
+        //     tags.push(`${tagPrefix}${major}.${minor}`)
+        // }
+        // console.log('tags', tags)
+        // if (!tags.length) {
+        //     core.notice('Major and Minor false, nothing to do!')
+        //     return
+        // }
+        //
+        // // Process Tags
+        // const octokit = github.getOctokit(githubToken)
+        // for (const tag of tags) {
+        //     core.info(`----- Processing tag: ${tag} -----`)
+        //     const ref = `tags/${tag}`
+        //     console.log('ref', ref)
+        //     const reference = await getRef(octokit, owner, repo, ref)
+        //     // console.log('reference', reference)
+        //     if (reference) {
+        //         if (sha !== reference.data.object.sha) {
+        //             console.log(`Updating tag: "${tag}" to sha: ${sha}`)
+        //             await updateRef(octokit, owner, repo, ref, sha)
+        //         } else {
+        //             console.log(`Tag: "${tag}" already points to sha: ${sha}`)
+        //         }
+        //     } else {
+        //         console.log(`Creating new tag: "${tag}" to sha: ${sha}`)
+        //         await createRef(octokit, owner, repo, ref, sha)
+        //     }
+        // }
 
-        // Process Tags
-        const octokit = github.getOctokit(githubToken)
-        for (const tag of tags) {
-            core.info(`----- Processing tag: ${tag} -----`)
-            const ref = `tags/${tag}`
-            console.log('ref', ref)
-            const reference = await getRef(octokit, owner, repo, ref)
-            // console.log('reference', reference)
-            if (reference) {
-                if (sha !== reference.data.object.sha) {
-                    console.log(`Updating tag: "${tag}" to sha: ${sha}`)
-                    await updateRef(octokit, owner, repo, ref, sha)
-                } else {
-                    console.log(`Tag: "${tag}" already points to sha: ${sha}`)
-                }
-            } else {
-                console.log(`Creating new tag: "${tag}" to sha: ${sha}`)
-                await createRef(octokit, owner, repo, ref, sha)
-            }
-        }
-
-        // core.setFailed('set to always fail for job retry')
+        core.setFailed('set to always fail for job retry')
     } catch (e) {
         core.debug(e)
         core.info(e.message)
